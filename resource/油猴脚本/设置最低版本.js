bdff4ca9b07d8ab955695f66a6bbee3f53d86271
Misaka <misaka@yangrucheng.top>
2025-09-06 22:07:49 +0800

    fix: script

--- resource/油猴脚本/设置最低版本.js
+++ resource/油猴脚本/设置最低版本.js
@@ -1,91 +1,98 @@
 // ==UserScript==
-// @name         微信小程序设置最低版本
+// @name         微信小程序快捷切换账号
 // @namespace    https://mp.weixin.qq.com/
-// @version      1.0.0
+// @version      1.0.1
 // @author       Misaka
 // @match        https://mp.weixin.qq.com/wxamp/*
 // @grant        none
-// @updateURL    https://cdn.oplist.org/gh/Misaka-1314/Chaoxing-MiniProgram@main/resource/油猴脚本/设置最低版本.js
-// @downloadURL  https://cdn.oplist.org/gh/Misaka-1314/Chaoxing-MiniProgram@main/resource/油猴脚本/设置最低版本.js
+// @updateURL    https://cdn.oplist.org/gh/Misaka-1314/Chaoxing-MiniProgram@main/resource/油猴脚本/账号切换.js
+// @downloadURL  https://cdn.oplist.org/gh/Misaka-1314/Chaoxing-MiniProgram@main/resource/油猴脚本/账号切换.js
 // ==/UserScript==
 
-(() => {
+const getQueryParam = (name) => {
+    const match = location.href.match(
+        new RegExp("[?&]" + name + "=([^&]*)")
+    );
+    return match ? decodeURIComponent(match[1]) : null;
+};
+
+const toFormData = (obj) => Object.keys(obj)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(obj[key]))
+    .join("&");
+
+const 获取小程序列表 = () => {
+    const random = Math.random();
+    const token = getQueryParam("token");
+
+    return new Promise(resolve => {
+        fetch(`https://mp.weixin.qq.com/wxamp/cgi/getWxaList?token=${token}&lang=zh_CN&random=${random}`)
+            .then(resp => resp.json())
+            .then(res => {
+                console.info("获取小程序列表", res);
+                if (res.wax_list.length > 0)
+                    resolve(res.wax_list.filter(item => item.type == 1).filter(item => !item.app_name.includes("测试号")))
+                else
+                    resolve([])
+            })
+    })
+}
+
+const 切换账号 = (username) => {
+    localStorage.setItem("_misaka_当前页面", window.location.href);
+    localStorage.setItem("_misaka_切换开关", "1");
+    const items = document.querySelectorAll(".account_item");
+    const item = Array.from(items).find(item => item.querySelector(".account_email").innerText === username);
+    console.info("找到账号元素", item);
+    item.click();
+}
+
+(async () => {
     "use strict";
 
-    const button = document.createElement("button");
-    button.id = "one-latest-version";
-    button.innerText = "设置最低版本~♡";
-    button.style.position = "fixed";
-    button.style.top = "60px";
-    button.style.right = "10px";
-    button.style.zIndex = "9999";
-    button.style.padding = "10px 15px";
-    button.style.backgroundColor = "#ff69b4";
-    button.style.color = "#fff";
-    button.style.border = "none";
-    button.style.borderRadius = "5px";
-    button.style.cursor = "pointer";
-
-    const toFormData = (obj) =>
-        Object.keys(obj)
-            .map(
-                (key) =>
-                    encodeURIComponent(key) + "=" + encodeURIComponent(obj[key])
-            )
-            .join("&");
-
-    const getQueryParam = (name) => {
-        const match = location.href.match(
-            new RegExp("[?&]" + name + "=([^&]*)")
-        );
-        return match ? decodeURIComponent(match[1]) : null;
-    };
-
-    button.addEventListener("click", async () => {
-        const random = Math.random();
+    const 切换开关 = localStorage.getItem("_misaka_切换开关");
+    if (切换开关 == "1") {
         const token = getQueryParam("token");
+        const 旧页面 = localStorage.getItem("_misaka_当前页面");
+        const url = new URL(旧页面);
+        url.searchParams.set("token", token);
+        const 新页面 = url.toString();
+        console.info("切换到旧页面", 新页面)
+        localStorage.setItem("_misaka_切换开关", "0");
+        window.location.href = 新页面;
+        return;
+    }
 
-        let count = 0;
-        document.getElementById("one-latest-version").disabled = true;
-        document.getElementById("one-latest-version").innerText = `正在执行~♡`;
-        document.getElementById("one-latest-version").style.backgroundColor =
-            "#006CBE";
-        setInterval(
-            () =>
-            (document.getElementById(
-                "one-latest-version"
-            ).innerText = `正在执行~♡ ${++count} s`),
-            1000
-        );
+    const 小程序列表 = await 获取小程序列表();
 
-        fetch(
-            `https://mp.weixin.qq.com/wxamp/cgi/config/basicConfig?token=${token}&lang=zh_CN&random=${random}`
-        )
-            .then((response) => response.json())
-            .then((result) => {
-                const historyList = result.historyList || [];
-                if (!historyList.length) return;
-                const version = historyList[0]?.version;
-                return fetch(
-                    `https://mp.weixin.qq.com/wxamp/cgi/setting/modifyMinWxaVersion?token=${token}&lang=zh_CN&random=${random}`,
-                    {
-                        method: "POST",
-                        body: toFormData({
-                            version: version,
-                        }),
-                        headers: {
-                            "Content-Type": "application/x-www-form-urlencoded",
-                            "Accept": "application/json, text/plain, */*",
-                        },
-                    }
-                );
-            })
-            .then((response) => response.json())
-            .then((result) => {
-                console.info("设置小程序版本号成功~♡", result)
-                window.location.href = `https://mp.weixin.qq.com/wxamp/basicprofile/index?token=${token}&lang=zh_CN&_=${random}`;
-            });
+    const container = document.createElement("div");
+    container.style.position = "fixed";
+    container.style.top = "110px";
+    container.style.right = "10px";
+    container.style.zIndex = "9999";
+    container.style.display = "flex";
+    container.style.flexDirection = "column";
+    container.style.alignItems = "flex-end";
+    container.style.gap = "4px";
+    container.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+
+    小程序列表.forEach((item, index) => {
+        const button = document.createElement("button");
+        button.innerText = item.app_name || item.username;
+        button.style.padding = "5px 8px";
+        button.style.width = "100px";
+        button.style.backgroundColor = "#072347";
+        button.style.color = "#fff";
+        button.style.fontSize = "12px";
+        button.style.border = "none";
+        button.style.borderRadius = "5px";
+        button.style.cursor = "pointer";
+        button.title = item.appid;
+        if (index)
+            button.addEventListener("click", () => 切换账号(item.username));
+        else
+            console.info("当前账号");
+        container.appendChild(button);
     });
 
-    document.body.appendChild(button);
+    document.body.appendChild(container);
 })();
