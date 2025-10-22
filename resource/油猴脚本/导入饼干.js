9ea4abcb8407719db69b4edd41ab541c3dd76ae1
Misaka <misaka@yangrucheng.top>
2025-10-22 16:53:41 +0800

    fix: script cookies import

--- resource/油猴脚本/导入饼干.js
+++ resource/油猴脚本/导入饼干.js
@@ -1,12 +1,9 @@
 // ==UserScript==
 // @name         超星 Cookie 导入助手
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
-// @description  在 chaoxing 页面右上角添加按钮导入 JSON 格式 Cookie
+// @version      1.0.1
+// @description  在 chaoxing 页面右上角添加按钮导入 JSON 格式 Cookie，仅主页面生效
 // @match        https://passport2.chaoxing.com/login*
-// @match        https://i.chaoxing.com/*
-// @updateURL    https://cdn.oplist.org/gh/Misaka-1314/Chaoxing-MiniProgram@main/resource/油猴脚本/导入饼干.js
-// @downloadURL  https://cdn.oplist.org/gh/Misaka-1314/Chaoxing-MiniProgram@main/resource/油猴脚本/导入饼干.js
 // @grant        none
 // ==/UserScript==
 
@@ -16,7 +13,7 @@
     if (window.top !== window.self) return; // 只在顶层执行
 
     const btn = document.createElement("button");
-    btn.innerText = "导入Cookies";
+    btn.innerText = "导入 Cookies";
     btn.style.position = "fixed";
     btn.style.top = "10px";
     btn.style.right = "10px";
@@ -31,16 +28,23 @@
     document.body.appendChild(btn);
 
     btn.addEventListener("click", () => {
-        const input = prompt(`请输入 JSON 格式的 Cookie：\n如 {"UID": "ab123"}`);
+        const input = prompt(`请输入 JSON 格式的 Cookie：\n如 {"uid": "ab123"}`);
         if (!input) return;
 
         const cookies = JSON.parse(input);
-        const domain = ".chaoxing.com";
+        const domains = [".chaoxing.com"];
         const path = "/";
 
-        for (const [key, value] of Object.entries(cookies))
-            document.cookie = `${key}=${encodeURIComponent(decodeURIComponent(value))}; domain=${domain}; path=${path}`;
-
-        window.location.href = "https://i.chaoxing.com/";
+        for (const domain of domains) {
+            for (const [key, value] of Object.entries(cookies)) {
+                document.cookie = `${key}=${encodeURIComponent(decodeURIComponent(value))}; domain=${domain}; path=${path}`;
+            }
+        }
+        fetch("https://sso.chaoxing.com/apis/login/userLogin4Uname.do")
+            .then(resp => resp.json())
+            .then(res => {
+                console.info(res);
+                window.location.href = "https://i.chaoxing.com/";
+            });
     });
 })();
